d196be60f78784598034105b31f3be8edac7651a
Bjørn Smith <bjornsmith@gmail.com>
2022-09-25 13:39:52 +0200

    Throw exception when command times out

--- Source/ROOT.Shared.Utils/OS/ProcessCall.cs
+++ Source/ROOT.Shared.Utils/OS/ProcessCall.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
 
 namespace ROOT.Shared.Utils.OS
 {
@@ -44,124 +42,6 @@ namespace ROOT.Shared.Utils.OS
         public static ProcessCall operator |(RemoteProcessCall first, ProcessCall second)
         {
             return first.Pipe(second);
-        }
-    }
-
-    public class ProcessReader : IDisposable
-    {
-        private readonly Process _process;
-        private readonly TimeSpan _timeout;
-        private readonly Stream _inputStream;
-        private StreamWriter _outputWriter;
-        private StreamWriter _errorWriter;
-        private object _factoryLock = new object();
-        private ManualResetEventSlim _outEvent = new ManualResetEventSlim(false);
-        private ManualResetEventSlim _errEvent = new ManualResetEventSlim(false);
-        public ProcessReader(Process process, TimeSpan timeout, Stream inputStream = null)
-        {
-            if (!process.StartInfo.RedirectStandardError
-                && !process.StartInfo.RedirectStandardOutput)
-            {
-                throw new ArgumentException("Process reader only works with processes that has been started with RedirectStandardOutput or RedirectStandardError", nameof(process));
-            }
-
-            if (inputStream != null && !process.StartInfo.RedirectStandardInput)
-            {
-                throw new ArgumentException($"If {nameof(inputStream)} is not null, then RedirectStandardInput must be true", nameof(process));
-            }
-
-            _process = process;
-            _timeout = timeout;
-            _inputStream = inputStream;
-            if (process.StartInfo.RedirectStandardError)
-            {
-                _errorWriter = new StreamWriter(new MemoryStream(), process.StartInfo.StandardErrorEncoding ?? Encoding.Unicode);
-            }
-
-            if (process.StartInfo.RedirectStandardOutput)
-            {
-                _outputWriter = new StreamWriter(new MemoryStream(), process.StartInfo.StandardInputEncoding ?? Encoding.Unicode);
-            }
-        }
-
-        private static void CopyStreamToProcessInput(Stream inputStream, Process process)
-        {
-            if (inputStream != null)
-            {
-                using var streamReader = new StreamReader(inputStream);
-
-                process.StandardInput.Write(streamReader.ReadToEnd());
-
-                //byte[] buffer = new byte[1024];
-                //int bytesRead = inputStream.Read(buffer, 0, 1024);
-                //while (bytesRead > 0)
-                //{
-                //    process.StandardInput.BaseStream.Write(buffer, 0, bytesRead);
-                //    bytesRead = inputStream.Read(buffer, 0, 1024);
-                //}
-            }
-        }
-
-        public bool Wait()
-        {
-            CopyStreamToProcessInput(_inputStream, _process);
-            _process.OutputDataReceived += (s, e) => Write(_outputWriter, e.Data, _outEvent);
-            _process.ErrorDataReceived += (s, e) => Write(_errorWriter, e.Data, _errEvent);
-            _process.Start();
-            _process.BeginErrorReadLine();
-            _process.BeginOutputReadLine();
-
-            bool success = _process.WaitForExit((int)_timeout.TotalMilliseconds);
-            if (!success)
-            {
-                _process.Kill();
-                _errorWriter.WriteLine($"Timed out after {_timeout}");
-            }
-
-            ExitCode = success ? _process.ExitCode : -1;
-
-            do
-            {
-            } while (!_outEvent.Wait(_timeout));
-
-            do
-            {
-            } while (!_errEvent.Wait(_timeout));
-
-            _outputWriter.Flush();
-            _errorWriter.Flush();
-            _outputWriter.BaseStream.Position = 0;
-            _errorWriter.BaseStream.Position = 0;
-
-            Output = new StreamReader(_outputWriter.BaseStream, _outputWriter.Encoding);
-            Error = new StreamReader(_errorWriter.BaseStream, _errorWriter.Encoding);
-            return success;
-        }
-
-        public int ExitCode { get; private set; }
-
-        public StreamReader Output { get; private set; }
-        public StreamReader Error { get; private set; }
-        private void Write(StreamWriter writer, string what, ManualResetEventSlim evt)
-        {
-            if (what == null)
-            {
-                evt.Set();
-                return;
-            }
-
-            lock (writer)
-            {
-                writer.WriteLine(what);
-            }
-        }
-
-        public void Dispose()
-        {
-            _outputWriter?.Dispose();
-            _errorWriter?.Dispose();
-            Output?.Dispose();
-            Error?.Dispose();
         }
     }
 
@@ -215,46 +95,16 @@ namespace ROOT.Shared.Utils.OS
             };
 
             using var process = new Process { StartInfo = startInfo };
-            using var reader = new ProcessReader(process, Timeout, inputStream);
+            using var reader = new ProcessReader(process, startInfo, Timeout, inputStream);
 
-            var success = reader.Wait();
+
+            reader.Wait();
             var exitCode = reader.ExitCode;
 
             var stdOut = reader.Output?.ReadToEnd();
             var stdError = reader.Error?.ReadToEnd();
 
             return new ProcessCallResult { ExitCode = exitCode, StdOut = stdOut, StdError = stdError, CommandLine = string.Join(" ", exec.BinPath, args) };
-
-
-            //process.StartInfo = startInfo;
-
-
-
-            //var output = new List<string>();
-            //var error = new List<string>();
-            //CopyStreamToProcessInput(inputStream, process);
-            //process.OutputDataReceived += (s, e) => Write(output, e.Data);
-            //process.ErrorDataReceived += (s, e) => Write(error, e.Data);
-            //process.Start();
-            //process.BeginErrorReadLine();
-            //process.BeginOutputReadLine();
-
-            //if (process.WaitForExit((int)Timeout.TotalMilliseconds))
-            //{
-
-            //    var exitCode = process.ExitCode;
-
-            //    var stdOut = string.Join("\r\n", output);
-            //    var stdError = string.Join("\r\n", error);
-            //    return new ProcessCallResult { ExitCode = exitCode, StdOut = stdOut, StdError = stdError, CommandLine = string.Join(" ", exec.BinPath, args) };
-            //}
-            //else
-            //{
-            //    process.Kill();
-            //    var stdOut = string.Join("\r\n", output);
-            //    var stdError = string.Join("\r\n", error);
-            //    return new ProcessCallResult { ExitCode = -1, StdOut = stdOut, StdError = $"Timed out after {Timeout}\r\n" + stdError, CommandLine = string.Join(" ", exec.BinPath, args) };
-            //}
         }
 
         private void Write(List<string> content, string what)
@@ -285,74 +135,6 @@ namespace ROOT.Shared.Utils.OS
         {
             return first.Pipe(second);
         }
-    }
-
-    public static class ProcessCallExtensions
-    {
-        public const string WindowsShell = "C:\\Windows\\System32\\cmd.exe";
-        public const string UnixShell = "/bin/bash";
-
-        private static readonly string Shell = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? WindowsShell : UnixShell;
-
-        public static ProcessCall ForceShell(this ProcessCall processCall, OSPlatform platform)
-        {
-            var shell = platform == OSPlatform.Windows ? WindowsShell : UnixShell;
-            return new ProcessCall(processCall.BinPath, processCall.Arguments) { Shell = shell };
-        }
-
-        public static ProcessCall Pipe(this ProcessCall processCall, ProcessCall other)
-        {
-            if (processCall is RemoteProcessCall remote)
-            {
-                return Pipe(remote, other);
-            }
-
-            if (processCall.Started || other.Started)
-            {
-                throw new InvalidOperationException("Cannot pipe two processes that has been started - use Pipe before you call LoadResponse");
-            }
-
-            string args = string.Concat(processCall.Arguments, " | ", other.BinPath, " ", other.Arguments).Trim();
-            return new ProcessCall(processCall.BinPath, args);
-        }
-
-        public static ProcessCall Pipe(this RemoteProcessCall processCall, ProcessCall other)
-        {
-            if (processCall.Started || other.Started)
-            {
-                throw new InvalidOperationException("Cannot pipe two processes that has been started - use Pipe before you call LoadResponse");
-            }
-
-            string args = string.Concat(processCall.Arguments, " \"", processCall.RequiresSudo ? Sudo.BinPath : "", " ", other.BinPath, " ", other.Arguments, "\"").Trim();
-
-            return new ProcessCall(processCall.BinPath, args);
-        }
-
-        public static ProcessCall Execute(this ProcessCall processCall)
-        {
-            if (processCall.Started)
-            {
-                throw new InvalidOperationException("Cannot execute  process that has been started");
-            }
-
-            if (!processCall.UseShell)
-            {
-                return processCall;
-            }
-            string args = string.Concat(ShellPrefix, " \"", processCall.BinPath, " ", processCall.Arguments,"\"").Trim();
-
-            var shell = processCall.Shell ?? Shell;
-            return new ProcessCall(shell, args);
-        }
-
-        public static ProcessCallException ToException(this ProcessCallResult result)
-        {
-            return new ProcessCallException(result.CommandLine, result.ExitCode, result.StdOut, result.StdError);
-        }
-
-        public const string WindowsShellPrefix = "/Q /C";
-        public const string UnixShellPrefix = "-c";
-        public static string ShellPrefix { get; set; }= RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? WindowsShellPrefix : UnixShellPrefix;
     }
 
     [Serializable]
